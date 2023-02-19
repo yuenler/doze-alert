1b0ea09616ad7c69cbe886a7ec7e3813d5140487
June Lee <jooeunjunelee@Junes-MacBook-Pro.local>
2023-02-18 21:31:45 -0800

    alert

--- src/Alert.js
+++ src/Alert.js
@@ -2,8 +2,11 @@ const Alert = () => {
   return (
     <div>
       <h1>Alert</h1>
+      Your level of activity has suggested that you may be tired. To avoid car injury, stop by a near rest stop or pull aside.
     </div>
   );
+
+<FontAwesomeIcon icon="fa-solid fa-arrow-left" />
 };
 
 export default Alert;
